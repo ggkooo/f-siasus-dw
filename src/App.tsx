21a48f341f9648c013289209fcbf9ad111f20121
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-04-13 15:55:07 -0300

    feat(app): configure routes and protected dashboard navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,62 @@
-function App() {
-  return (
-    <>
-    
-    </>
-  )
-}
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import LoginPage from './pages/LoginPage';
+import OverviewPage from './pages/OverviewPage';
+import CompetenciaPage from './pages/CompetenciaPage';
+import MunicipioPage from './pages/MunicipioPage';
+import ProcedimentoPage from './pages/ProcedimentoPage';
+import CBOPage from './pages/CBOPage';
 
-export default App
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <OverviewPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/competencia"
+            element={
+              <PrivateRoute>
+                <CompetenciaPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/municipio"
+            element={
+              <PrivateRoute>
+                <MunicipioPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/procedimento"
+            element={
+              <PrivateRoute>
+                <ProcedimentoPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/cbo"
+            element={
+              <PrivateRoute>
+                <CBOPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
